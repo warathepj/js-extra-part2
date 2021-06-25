0c32bbd796f264131e73b33568f8267b50f4a149
warathepj <80021220+warathepj@users.noreply.github.com>
2021-06-25 15:02:38 +0700

    11 16

--- strings.js
+++ strings.js
@@ -1,9 +1,11 @@
 
-const word = 'Black Earring';
-const searchWord = 'Ear';
-const isFound = word.includes(searchWord);
-console.log(isFound);
+// const word = 'Black Earring';
+// const searchWord = 'ear';
+// const isFound = word.toLowerCase().includes(searchWord.toLowerCase());
+// console.log(isFound);
 
 
 
 const productCode = 'th-926405';
+const country = productCode.slice(0, 2);
+const productId = productCode.slice(3);
